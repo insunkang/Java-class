5bbbd11aec843e819fe703e75aed4df9bca1baf1
insunkang <cactus2378@naver.com>
2019-12-06 16:51:24 +0900

    12/6 array

--- src/chapt05/ArrayTest01.java
+++ src/chapt05/ArrayTest01.java
@@ -4,11 +4,35 @@ public class ArrayTest01 {
 	public static void main(String[] args) {
 		//1. 배열의 선언
 		int[] myarr;
+		int[] myarr2;
 		//2. 배열의 생성
 		myarr = new int[5];
-		
+		myarr2= new int[5];
 		System.out.println(myarr);
-
+		System.out.println(myarr2);
+		System.out.println("myarr의 2번 요소의 값 =>"+myarr[2]);
+		
+		//3. 배열의 초기화 - 2번 요소에 100을 저장
+		myarr[2] = 100;
+		System.out.println("myarr의 2번 요소의 값=>"+myarr[2]);
+		
+		//배열의 선언과 생성을 한거번에 처리
+		int[] numarr  = new int[3];
+		boolean[] boolarr  = new boolean[5];
+		long[] longarr = new long[100];
+		double[] dbarr = new double[1000];
+		
+		
+		//배열의 기본 초기값
+		//기본형인 경우
+		//정수형 - 0
+		//boolean  - false
+		//실수형 -  0.0
+		System.out.println(numarr[0]);
+		System.out.println(boolarr[0]);
+		System.out.println(longarr[0]);
+		System.out.println(dbarr[0]);
+		
 	}
 
 }
